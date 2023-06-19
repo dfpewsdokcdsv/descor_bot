210272c28b74509454b2c4b36f85a1c1de89df9f
straz <132742180+straz1ki@users.noreply.github.com>
2023-06-19 21:58:02 +0600

    Update botscontent.py

--- botscontent.py
+++ botscontent.py
@@ -21,5 +21,17 @@ async def heh(ctx, count_heh = 5):
 async def random_number(ctx):
     number = random.randint(1, 10)
     await ctx.send(f'The random number is: {number}')
-    
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member):
+    await member.ban()
+    await ctx.send(f'{member.name} был забанен!')
+
+@bot.command()
+@commands.has_permissions(manage_roles=True)
+async def mute(ctx, member: discord.Member):
+    muted_role = discord.utils.get(ctx.guild.roles, name='Muted')
+    await member.add_roles(muted_role)
+    await ctx.send(f'{member.name} был замучен!')
+
 bot.run("token")
